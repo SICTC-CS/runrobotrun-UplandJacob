894869ed30d1d79f4dd36ae2704655316e4c4d5a
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2026-02-02 12:38:40 -0600

    rev 3 - another maze and better functions

---
+++ Rev3/robot_maze.py
@@ -0,0 +1,88 @@
+#   a115_robot_maze.py
+import turtle as trtl
+
+#----- maze and turtle config variables
+screen_h = 400
+screen_w = 420
+startx = -100
+starty = -100
+turtle_scale = 1.5
+
+#------ robot commands
+def move(n=1):
+	for _ in range(n):
+		robot.dot(10)
+		robot.fd(50)
+
+def turn_left():
+	robot.speed(0)
+	robot.lt(90)
+	robot.speed(2)
+def turn_right():
+	robot.speed(0)
+	robot.rt(90)
+	robot.speed(2)
+
+#----- init screen
+wn = trtl.Screen()
+wn.setup(width=screen_w, height=screen_h)
+robot_image = "assets/robot.gif"
+wn.addshape(robot_image)
+
+#----- init robot
+robot = trtl.Turtle(shape=robot_image)
+robot.hideturtle()
+robot.color("darkorchid")
+robot.pencolor("darkorchid")
+robot.penup()
+robot.setheading(90)
+robot.turtlesize(turtle_scale, turtle_scale)
+robot.goto(startx, starty)
+robot.speed(2)
+robot.showturtle()
+
+while True:
+	for maze in range(1, 4+1):
+		wn.bgpic(f"assets/maze{maze}.png")
+		match maze:
+			case 1:
+				move(4)
+				turn_right()
+				move(4)
+			case 2:
+				move(3)
+				turn_right()
+				move(2)
+			case 3:
+				for i in range(8):
+					move()
+					if i % 2:
+						turn_left()
+					else:
+						turn_right()
+					if i == 4:
+						robot.pencolor("red")
+			case 4:
+				turn_right()
+				move()
+				turn_left()
+				move(2)
+				turn_left()
+				move()
+				turn_right()
+				move(2)
+				turn_right()
+				move(4)
+				turn_right()
+				move(2)
+				turn_right()
+				move()
+				turn_left()
+				move(2)
+				turn_left()
+				move()
+		# reset
+		robot.clear()
+		robot.pencolor("darkorchid")
+		robot.goto(startx, starty)
+		robot.setheading(90)
